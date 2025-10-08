6712d989c81b84f437ad4c9b34a3b21e43194f60
Nathan Ngo <nathan_ngo@ucsb.edu>
2025-10-07 17:52:02 -0700

    xy - updated with actual github id

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -25,6 +25,10 @@ public class DeveloperTest {
         assertEquals("Nathan N.", Developer.getName());
     }
 
+    public static void getGithubId_returns_correct_githubId() {
+        assertEquals("ngostream", Developer.getGithubId());
+    }
+
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 
