205ab0f8b1313ce9be0cd4a8a74adc431e15c9bb
Nathan Ngo <nathan_ngo@ucsb.edu>
2025-10-07 23:32:12 -0700

    nn - add tests to increase test coverage and mutation test quality

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -19,8 +19,61 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
-   
-    // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
-    // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
 
+    @Test
+    public void equals_same_obj() {
+        assertEquals(true, team.equals(team));
+    }
+
+    @Test
+    public void equals_not_team_obj() {
+        int n = 0;
+        assertEquals(false, team.equals(n));
+    }
+
+    @Test
+    public void equals_both_and_eq() {
+        Team team1 = new Team("test-team");
+        assertEquals(true, team.equals(team1));
+    }
+
+    @Test
+    public void equals_none_and_eq() {
+        Team team1 = new Team("wrong-team");
+        team1.addMember("Nathan N.");
+        assertEquals(false, team.equals(team1));
+    }
+
+    @Test
+    public void equals_one_and_eq() {
+        Team team1 = new Team("test-team");
+        team.addMember("Nathan N.");
+        assertEquals(false, team.equals(team1));
+
+        Team team2 = new Team("wrong-team");
+        assertEquals(false, team.equals(team2));
+    }
+
+    @Test
+    public void hash_code_test() {
+        Team t1 = new Team();
+        t1.setName("foo");
+        t1.addMember("bar");
+        Team t2 = new Team();
+        t2.setName("foo");
+        t2.addMember("bar");
+        assertEquals(t1.hashCode(), t2.hashCode());
+    }
+
+    @Test
+    public void hash_code_test_specific() {
+        Team t = new Team("");
+        int result = t.hashCode();
+        int expectedResult = 1;
+        assertEquals(expectedResult, result);
+    }
 }
